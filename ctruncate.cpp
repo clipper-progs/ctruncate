80fc6f0374fd905d28b6dc585efa659a018dce2e
Charles Ballard <charles.ballard@stfc.ac.uk>
2015-12-15 17:58:22 +0000

    correct date

--- ctruncate.cpp
+++ ctruncate.cpp
@@ -58,7 +58,7 @@ int main(int argc, char **argv)
 {
     clipper::String prog_string = "ctruncate";
     clipper::String prog_vers = "1.17.7";
-    clipper::String prog_date = "$Date: 2015/13/07";
+    clipper::String prog_date = "$Date: 2015/12/07";
 	ctruncate::CCP4Program prog( prog_string.c_str(), prog_vers.c_str(), prog_date.c_str() );
     
     // defaults
