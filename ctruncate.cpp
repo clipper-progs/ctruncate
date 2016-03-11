ef22d62e6f21b75969953e7dd7aeee593bdf0e05
Charles Ballard <charles.ballard@stfc.ac.uk>
2016-03-11 15:43:23 +0000

    1.17.10, rationalise Scattering and WilsonB

--- ctruncate.cpp
+++ ctruncate.cpp
@@ -57,8 +57,8 @@ using namespace ctruncate;
 int main(int argc, char **argv)
 {
     clipper::String prog_string = "ctruncate";
-    clipper::String prog_vers = "1.17.9";
-    clipper::String prog_date = "$Date: 2016/03/01";
+    clipper::String prog_vers = "1.17.10";
+    clipper::String prog_date = "$Date: 2016/03/10";
 	ctruncate::CCP4Program prog( prog_string.c_str(), prog_vers.c_str(), prog_date.c_str() );
     
     // defaults
--- ctruncate_wilson.h
+++ ctruncate_wilson.h
@@ -21,15 +21,29 @@ namespace ctruncate {
 	class Scattering {
 	public:
 		enum MODE { PROTEIN, NUCLEIC, COMPLEX };
+        enum AtomType_t { H = 0, C = 5, N = 6, O = 7, P=14, S=15 };
+
 		Scattering( MODE mode=PROTEIN) : _mode(mode) { init(); }
 		~Scattering() {}
-		clipper::ftype operator()(const int nres, clipper::ftype power=2.0 );
-		clipper::ftype operator()(const clipper::MPolymerSequence& poly, clipper::ftype power=2.0);
+        
+        MODE mode() const { return _mode; }
+        MODE mode( MODE mode) { MODE tmp = _mode; _mode = mode; return tmp; }
+        
+        clipper::ftype numAtoms(const std::string&) const;
+        clipper::ftype numAtoms(int) const;
+        
+        clipper::ftype f(const clipper::ftype invresolsq, clipper::ftype power=2.0) const;
+        
+        clipper::ftype operator()(const clipper::HKL_data_base& data, const int nres, clipper::ftype power=2.0);
+        clipper::ftype operator()(const clipper::HKL_data_base& data, const clipper::MPolymerSequence& poly, clipper::ftype power=2.0);
+        clipper::ftype operator()(const clipper::HKL_data_base& data, clipper::ftype solvent, clipper::ftype power=2.0);
+        clipper::ftype operator()(const clipper::HKL_data_base& data, clipper::MMDBManager& mmdb, clipper::ftype power=2.0);
+        clipper::ftype operator()(const clipper::HKL_data_base& data, std::vector< clipper::Atom>& atoms, clipper::ftype power=2.0);
+		clipper::ftype operator()(const int nres, const clipper::Spacegroup& spg, clipper::ftype power=2.0 );
+		clipper::ftype operator()(const clipper::MPolymerSequence& poly, const clipper::Spacegroup& spg, clipper::ftype power=2.0);
 		clipper::ftype operator()(const clipper::Cell& cell, const clipper::Spacegroup& spg, clipper::ftype solvent=0.5, clipper::ftype power=2.0 );
 		clipper::ftype operator()(clipper::MMDBManager& mmdb, const clipper::Spacegroup& spg, clipper::ftype power=2.0);
 		clipper::ftype operator()(std::vector<clipper::Atom>& mmdb, const clipper::Spacegroup& spg, clipper::ftype power=2.0 );
-		
-		static const clipper::ftype C, O, N, H, S, P;
 		
 	private:
 		void init();
@@ -52,8 +66,9 @@ namespace ctruncate {
 		static const clipper::ftype NucleicComp[5]; 
 		
         MODE _mode;                                   // mode of operation
-		std::vector<std::string> atomtype;
-		std::vector<clipper::ftype> scattering;
+		std::vector<std::string> atomtype;            // name of scatterer
+		std::vector<clipper::ftype> scattering;       // number of electrons per scatter
+        std::vector<clipper::ftype> numatoms;         // number of atomtype in unit cell
 		
 	};
 	
@@ -61,8 +76,8 @@ namespace ctruncate {
 	public:
 		enum MODE {STRAIGHT, BEST, RNA };
 		
-		WilsonB ( WilsonB::MODE _mode = BEST ) : mode(_mode), nresidue_supplied(false), sequence_supplied(false), _a(-1.0), _totalscat(0.0)
-		{ numatoms.resize(6); }
+		WilsonB ( WilsonB::MODE _mode = BEST ) : mode(_mode), nresidue_supplied(false), sequence_supplied(false), _a(-1.0), _totalscat(300,0.0)
+        { scattering.mode((mode == RNA ) ? Scattering::NUCLEIC : Scattering::PROTEIN ); }
 		
 		clipper::ftype operator()(const clipper::HKL_data_base& data, const clipper::Range<clipper::ftype>* range = NULL, ctruncate::Rings* ice = NULL);
 		
@@ -86,7 +101,8 @@ namespace ctruncate {
         clipper::ftype f(clipper::HKL_data_base::HKL_reference_index&);
 		
 	private:
-		MODE mode;
+        ctruncate::Scattering scattering;   //hold information on cell contents
+		MODE mode;          //mode
         clipper::ftype _a;  // slope
         clipper::ftype _b;  // intercept
         clipper::ftype _siga; // uncertainty in intercept
@@ -94,28 +110,20 @@ namespace ctruncate {
 		const clipper::HKL_data_base* intensity;
 		clipper::Range<clipper::ftype> activeRange; //range of data used in calculation
         
-        
-		
 		bool nresidue_supplied, sequence_supplied; //book keeping
 		int nresidues; // number of residues
-		std::vector<int> numatoms;  // number of atoms
-		int _totalscat;  //number of scattering electrons in cell (scale for BEST)
+		//std::vector<int> numatoms;  // number of atoms
+        std::vector<clipper::ftype> _totalscat;  //number of scattering electrons in cell (scale for BEST)
 		float maxres;
 		
-		static const std::string AtomNames[];  //atom names
-		static const char ResidueNames[];  //residue names
-		static const int Catoms[];         // number of C per residue
-		static const int Hatoms[];         // number of H per residue
-		static const int Natoms[];         // number of N per residue
-		static const int Oatoms[];         // number of O per residue
-		static const int Satoms[];         // number of S per residue
-		static const int Patoms[];         // number of P per residue
+        static const std::string AtomNames[];
 		
 		clipper::ftype obs(const clipper::HKL_data_base&, const clipper::HKL_data_base::HKL_reference_index&);              // return observed value as I or f*f
 		clipper::ftype sigobs(const clipper::HKL_data_base&, const clipper::HKL_data_base::HKL_reference_index&);           // return suitable sigma
 		void wilson_straight(const clipper::HKL_data_base&, clipper::Range<clipper::ftype>& range, ctruncate::Rings& ice);
 		void wilson_best(const clipper::HKL_data_base&, clipper::Range<clipper::ftype>& range, ctruncate::Rings& ice);
 		void wilson_rna(const clipper::HKL_data_base&, clipper::Range<clipper::ftype>& range, ctruncate::Rings& ice);
+        
 	};
 		
 }
